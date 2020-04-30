05942c3e4dcfff034ea4011b75d5af981b373e3c
DJ <dj@walker.dev>
2020-04-30 11:28:03 -0400

    add Tina provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/index.css'
+import { withTina } from 'tinacms'
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default withTina(MyApp)
