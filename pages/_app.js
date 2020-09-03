762d28de3dfb59db17deacf4bc5d07bbe1f811f0
DJ <dj@walker.dev>
2020-09-03 11:05:00 -0400

    add markdown plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/index.css'
 import { withTina } from 'tinacms'
+import { MarkdownFieldPlugin } from 'react-tinacms-editor'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default withTina(MyApp, { enabled: true, sidebar: true })
+export default withTina(MyApp, {
+  enabled: true,
+  sidebar: true,
+  plugins: [MarkdownFieldPlugin],
+})
